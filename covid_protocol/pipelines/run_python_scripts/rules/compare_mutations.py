354cd78229c1f991afa01d2bdddc93d08c74708e
klara <sladeckova.klara@gmail.com>
2021-04-15 11:36:10 +0000

    added probability (veeeeeery simplified) and loading submutations

--- covid_protocol/pipelines/run_python_scripts/rules/compare_mutations.py
+++ covid_protocol/pipelines/run_python_scripts/rules/compare_mutations.py
@@ -35,23 +35,37 @@ def load_file(file_path, reference):
             barcode_dict[barcode][position][3]+=T   
     return barcode_dict
 
+
 def load_mutations(mutations_path):    
     #nacitanie mut.txt
     
-    mut_number = {}
     mutations = {}
+    sub_mutations = {}
     
     with open (mutations_path, "r") as txtfile:
+        sub = False
+        mut_name = ""
         for line in txtfile:
             line = line.strip()
-            if not line.startswith('#') and line:
+            if line == "start_sub":
+                sub = True
+            elif line == "end_sub":
+                sub = False
+            elif sub:
                 columns = line.split(" ")
-                mut_number[columns[0]] = columns[1]
-                mutations[columns[0]] = []
-                mutations[columns[0]].append(int(columns[1]))
-                mutations[columns[0]].append([])
+                sub_mutations[mut_name].append([])
+                index = len(sub_mutations)-1
+                for i in range (2, len(columns)):
+                    sub_mutations[mut_name][index].append(columns[i])
+                
+            elif not line.startswith('#') and line:
+                columns = line.split(" ")
+                mut_name = columns[0]
+                mutations[mut_name] = []
+                mutations[mut_name].append(int(columns[1]))
+                mutations[mut_name].append([])
                 for i in range(2, len(columns)):
-                    mutations[columns[0]][1].append(columns[i])
+                    mutations[mut_name][1].append(columns[i])
             
 
     return mutations
@@ -61,9 +75,11 @@ def guess(barcode_dict, mutacie, csv, threshold):
     #barcode dict - nacitany dictionary zo suboru (pocty ACGT na jednotlivych poziciach v jednotlivych barkodoch)
     #mutacie - nacitany subor mut.txt (load_mutations)
     #print(barcode_dict)
-    print("barcode,strand,support,mutations", file=csv)
+    print("barcode,strand,support,mutations,probability", file=csv)
     for barcode in barcode_dict:
         for strand in mutacie:
+            probability = 0.0
+            num_mut = 0
             pocet = 0
             support = []
             mutations_string = "" #mutacie sconcatenovane cez &
@@ -71,11 +87,16 @@ def guess(barcode_dict, mutacie, csv, threshold):
             for mut in mutacie[strand][1]:
                 #print(mut)
                 if mut[2].isdigit():
+                    num_mut += 1
                     position = int(mut[1:len(mut)-1])-1
                     m1 = barcode_dict[barcode][position][l2n[mut[0]]] #pocet baz zhodnych s referenciou
                     m2 = barcode_dict[barcode][position][l2n[mut[len(mut)-1]]]; #pocet baz zhodnych s mutaciou
                     reads_coverage = barcode_dict[barcode][position][0]+barcode_dict[barcode][position][1]+barcode_dict[barcode][position][2]+barcode_dict[barcode][position][3]
-                    if m1 < m2 and reads_coverage >= threshold: #ak bolo na danu poziciu namapovanych uz aspon threshold baz a je viac tych zmutovanych 
+                   
+                    if reads_coverage != 0:
+                    	probability += m2/reads_coverage 
+                    	
+                    if m1 < m2 and reads_coverage >= threshold: #ak bolo na danu poziciu namapovanych uz aspon threshold baz a je viac tych zmutovanych                         
                         pocet += 1
                         support.append([mut, m1, m2])
                         if first:
@@ -84,8 +105,7 @@ def guess(barcode_dict, mutacie, csv, threshold):
                         else:
                             mutations_string+="&"+mut
             if (pocet > mutacie[strand][0]):
-                print(barcode+","+strand+","+str(pocet)+","+mutations_string, file=csv)
-            
+                print(barcode+","+strand+","+str(pocet)+","+mutations_string+","+str(probability/num_mut), file=csv)
     
               
             
